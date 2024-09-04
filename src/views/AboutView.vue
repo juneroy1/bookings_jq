fd02845660fed99edec4fc40f86a21e9e9efe1c5
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-04 13:18:12 +0800

    feat: import css class for display flex

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="about">
+  <div class="about display-flex">
     <h1>List booking page</h1>
     <button>Create Booking</button>
   </div>
 </template>
+<script setup>
+import '../assets/css/style.css'
+
+</script>
