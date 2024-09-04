129a21100566ad07a9c1e358abc09df9db5033ed
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-04 22:42:41 +0800

    feat append booking create form in the form page booking

--- src/views/Booking/FormPage.vue
+++ src/views/Booking/FormPage.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
-
+        <BookingItemForm/>
     </div>
 </template>
 
 <script >
+import BookingItemForm from './components/ItemForm.vue'
 export default {
+    components: {
+        BookingItemForm
+    },
     setup(){
-        
+
     }
 }
 </script>
