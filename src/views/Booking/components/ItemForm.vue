9bb3d386d6e8aa079558d5229a553360ca65ae99
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-04 22:42:26 +0800

    feat: create booking create form

---
+++ src/views/Booking/components/ItemForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{ test }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BookingItemForm",
+    setup () {
+        const test = 'hello test'
+
+        return {test}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
