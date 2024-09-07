e71c4e07b601c012ab3860b3b4fd54f38d770e2f
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-07 22:59:30 +0800

    feat issue warning for the table

--- src/views/BookingsList.vue
+++ src/views/BookingsList.vue
@@ -1,49 +1,54 @@
 <template>
   <div class="about display-flex">
     <h1>List booking page</h1>
-    <button class="btn btn-primary" @click="createNewBooking()">Create Booking</button>
+    <button class="btn btn-primary" @click="createNewBooking()">
+      Create Booking
+    </button>
     <div class="sizeBox-50"></div>
     <table class="width-100-per">
-      <th>Stall holder</th>
-      <th>Booked Item</th>
-      <th>Quantity</th>
-      <th>Total Tax</th>
-      <th>Total Cost</th>
-      <tr>
-        <td>Juneroy</td>
-        <td>ITEM 1</td>
-        <td>10</td>
-        <td>$10.00</td>
-        <td>$100.00</td>
-      </tr>
-      <tr>
-        <td>Quinimon</td>
-        <td>ITEM 2</td>
-        <td>20</td>
-        <td>$20.00</td>
-        <td>$200.00</td>
-      </tr>
+      <thead>
+        <tr>
+          <th>Stall holder</th>
+          <th>Booked Item</th>
+          <th>Quantity</th>
+          <th>Total Tax</th>
+          <th>Total Cost</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Juneroy</td>
+          <td>ITEM 1</td>
+          <td>10</td>
+          <td>$10.00</td>
+          <td>$100.00</td>
+        </tr>
+        <tr>
+          <td>Quinimon</td>
+          <td>ITEM 2</td>
+          <td>20</td>
+          <td>$20.00</td>
+          <td>$200.00</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
-<script >
-import { onMounted, ref } from 'vue';
-import '../assets/css/style.css'
-import router from '@/router';
+<script>
+import { onMounted, ref } from "vue";
+import "../assets/css/style.css";
+import router from "@/router";
 
-export default{
-  setup(){
+export default {
+  setup() {
     const bookings = ref([]);
 
     const createNewBooking = () => {
-      router.push('/booking/create')
-    }
-    onMounted(() =>{
-      
-    })
-    
-    return {bookings,createNewBooking}
-  },
-}
+      router.push("/booking/create");
+    };
+    onMounted(() => {});
 
+    return { bookings, createNewBooking };
+  },
+};
 </script>
