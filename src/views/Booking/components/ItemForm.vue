e2eea293296b785f11d0ca9a9f0d1bf695c7806f
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-07 22:48:30 +0800

    feat created new select drop down for selecting stallholder

--- src/views/Booking/components/ItemForm.vue
+++ src/views/Booking/components/ItemForm.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>
-       
+    <div class="header">
+        <h2>Create new booking</h2>
     </div>
+        <div class="form">
+            <div class="container">
+                <label for="stallholder">Stallholder:</label>
+                <select name="stallholder" id="stallholder">
+                    <option value="1">Juneroy</option>
+                    <option value="2">Quinimon</option>
+                    <option value="3">Derano</option>
+                </select>
+            </div>
+        </div>
 </template>
 
 <script>
