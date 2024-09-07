3711348d08722fcd79da555381f6c27515d3eff4
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-07 23:11:03 +0800

    feat apply action button on the form page booking

--- src/views/Booking/FormPage.vue
+++ src/views/Booking/FormPage.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
         <BookingItemForm/>
+        <ActionButton/>
     </div>
 </template>
 
 <script >
+import ActionButton from '../components/ActionButton.vue';
 import BookingItemForm from './components/ItemForm.vue'
 export default {
     components: {
-        BookingItemForm
+        BookingItemForm,
+        ActionButton
     },
     setup(){
 
