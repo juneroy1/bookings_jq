ec3c2e7ebbd46722ed6ba023df5272ad6f9538fd
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-07 22:54:48 +0800

    feat apply booking object value into the forms

--- src/views/Booking/components/ItemForm.vue
+++ src/views/Booking/components/ItemForm.vue
@@ -2,10 +2,11 @@
     <div class="header">
         <h2>Create new booking</h2>
     </div>
+    {{ booking }}
         <div class="form">
             <div class="container">
                 <label for="stallholder">Stallholder:</label>
-                <select name="stallholder" id="stallholder">
+                <select name="stallholder" v-model="booking.stallholder" id="stallholder">
                     <option value="1">Juneroy</option>
                     <option value="2">Quinimon</option>
                     <option value="3">Derano</option>
@@ -13,7 +14,7 @@
             </div>
             <div class="container">
                 <label for="market_date">Market date:</label>
-                <select name="market_date" id="market_date">
+                <select name="market_date" v-model="booking.market_date" id="market_date">
                     <option value="1">Market 1</option>
                     <option value="2">Market 2</option>
                     <option value="3">Market 3</option>
@@ -23,12 +24,18 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
     name: "BookingItemForm",
     setup () {
-        const test = 'hello test'
+        
+        const booking = ref({
+            stallholder:null,
+            market_date:null,
+        })
 
-        return {test}
+        return {booking}
     }
 }
 </script>
