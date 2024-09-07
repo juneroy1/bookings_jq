f6dec206d503b0801ca74b5ec8cbb686bfdee613
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-07 23:25:20 +0800

    feat: implement props bookings in the form

--- src/views/Booking/FormPage.vue
+++ src/views/Booking/FormPage.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
-        <BookingItemForm/>
+        {{ booking }}
+        <BookingItemForm
+        :booking="booking"
+        />
         <ActionButton/>
     </div>
 </template>
 
 <script >
+import { ref } from 'vue';
 import ActionButton from '../components/ActionButton.vue';
 import BookingItemForm from './components/ItemForm.vue'
 export default {
@@ -14,7 +18,16 @@ export default {
         ActionButton
     },
     setup(){
+        const booking = ref({
+      stallholder: null,
+      market_date: null,
+      total_cost_ex_tax: null,
+      total_tax: null,
+      total_cost_inc_tax: null,
+      comment: null,
+    });
 
+    return { booking };
     }
 }
 </script>
